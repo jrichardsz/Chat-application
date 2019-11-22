06c21c7359ab43d523cbb1fa1924f1c7215d06ee
ayesha kaleem <akaleem306@gmail.com>
2019-11-22 17:31:19 +0500

    some changes

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ type Message struct {
 }
 func main() {
 	// Create a simple file server
-	fs := http.FileServer(http.Dir("../public"))
+	fs := http.FileServer(http.Dir("public"))
 	http.Handle("/", fs)
 	// Configure websocket route
 	http.HandleFunc("/ws", handleConnections)
